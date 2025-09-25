7face18cd9a05286fbf9430da9c34e5401d1b1f3
Matthew <mattyHoseini@gmail.com>
2025-09-25 07:34:44 -0700

    feat(landing): add typewriter effect and animated counters for courses, minutes and users

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 const $ = document;
 const landingTitle = $.querySelector(".landing__title");
+const landingCoursesCount = $.querySelector("#courses-count");
+const landingMinutesCount = $.querySelector("#minutes-counter");
+const landingUsersCount = $.querySelector("#users-counter");
 
 window.addEventListener("load", () => {
-  let landingText = "ما به هر قیمتی دوره آموزشی تولید نمیکنیم";
+  let landingText = "ما به هر قیمتی دوره آموزشی تولید نمی کنیم !";
   let typeIndex = 0;
+
   typeWriter(landingText, typeIndex);
+  makeCounter(40, landingCoursesCount);
+  makeCounter(3_320, landingMinutesCount);
+  makeCounter(3_071, landingUsersCount);
 });
 
 function typeWriter(text, index) {
@@ -17,4 +24,15 @@ function typeWriter(text, index) {
     typeWriter(text, index);
   }, 100);
 }
-console.log(landingTitle);
+
+function makeCounter(max, elem) {
+  let counter = 0;
+  const interval = setInterval(() => {
+    if (counter === max) {
+      clearInterval(interval);
+    }
+
+    elem.innerHTML = counter;
+    counter++;
+  }, 0.5);
+}
